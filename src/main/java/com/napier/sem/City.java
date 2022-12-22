e588f847f5b3ed7483faa262c18c36f9b2da10fd
Banyar-Po2 <40524300@napier.ac.uk>
2022-12-22 14:01:13 +0630

    Added Getter and Setter for City

--- src/main/java/com/napier/sem/City.java
+++ src/main/java/com/napier/sem/City.java
@@ -3,10 +3,41 @@ package com.napier.sem;
 public class City {
 
     private String city_name; // city name
+    public String getCity_name() {
+        return city_name;
+    }
+
+    public void setCity_name(String city_name) {
+        this.city_name = city_name;
+    }
 
     private String city_countryCode; // country code
 
+    public String getCity_countryCode() {
+        return city_countryCode;
+    }
+
+    public void setCity_countryCode(String city_countryCode) {
+        this.city_countryCode = city_countryCode;
+    }
+
     private String city_district; // district name
+    public String getCity_district() {
+        return city_district;
+    }
+
+    public void setCity_district(String city_district) {
+        this.city_district = city_district;
+    }
 
     private int city_population; // city population
+    public int getCity_population() {
+        return city_population;
+    }
+
+    public void setCity_population(int city_population) {
+        this.city_population = city_population;
+    }
+
+
 }
---
+++ src/main/java/com/napier/sem/city_info.java
@@ -0,0 +1,10 @@
+package com.napier.sem;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class city_info {
+
+}
